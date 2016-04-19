91cc9ee6a7aefa98c8cea34cdb1e54a04f589bb4
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-04-19 19:03:12 +0900

    Update

--- android_connect.sh
+++ android_connect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 adb kill-server
 adb start-server
-TARGET_PORT=5555
+# TARGET_PORT=5555
+TARGET_PORT=18188
 IP=`adb shell netcfg |grep wlan0 |egrep -o '[0-9]+(\.[0-9]+){3}'`
 echo "Android-IP is $IP"
 adb tcpip $TARGET_PORT
